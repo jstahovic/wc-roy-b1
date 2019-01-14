1081f388f3ed8386201b0729550abc0776feb32a
jstahovic <jstahovicjr@gmail.com>
2019-01-13 23:29:33 -0500

    Added the wildcard route to route to VoidComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
+import { VoidComponent } from './void/void.component'
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
-  {
-    path: 'red',
-    loadChildren: './red/red.module#RedModule',
-  },
+  { path: 'red', loadChildren: './red/red.module#RedModule' },
+  { path: '**', component: VoidComponent },
 ]
 
 @NgModule({
