815327ac478f994156b46a5aa5dcf456319a947c
jstahovic <jstahovicjr@gmail.com>
2019-01-13 23:42:43 -0500

    Generated the MediumRedComponent and DarkRedComponent and added them RedModule's routing table.

--- src/app/red/red-routing.module.ts
+++ src/app/red/red-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { DarkRedComponent } from './dark-red/dark-red.component'
 import { LightRedComponent } from './light-red/light-red.component'
+import { MediumRedComponent } from './medium-red/medium-red.component'
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'light-red' },
   { path: 'light-red', component: LightRedComponent },
+  { path: 'medium-red', component: MediumRedComponent },
+  { path: 'dark-red', component: DarkRedComponent },
 ]
 
 @NgModule({
--- src/app/red/red.module.ts
+++ src/app/red/red.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { DarkRedComponent } from './dark-red/dark-red.component'
 import { LightRedComponent } from './light-red/light-red.component'
+import { MediumRedComponent } from './medium-red/medium-red.component'
 import { RedRoutingModule } from './red-routing.module'
 
 @NgModule({
-  declarations: [LightRedComponent],
+  declarations: [LightRedComponent, MediumRedComponent, DarkRedComponent],
   imports: [CommonModule, RedRoutingModule],
 })
 export class RedModule {}
