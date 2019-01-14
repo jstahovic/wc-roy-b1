b4a34dd186ffbf5595f21c6ffca23fb3742a417e
jstahovic <jstahovicjr@gmail.com>
2019-01-13 23:01:26 -0500

    Added home and void components.  Updated scratch.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { VoidComponent } from './void/void.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, VoidComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
