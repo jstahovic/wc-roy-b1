6071135a6ade4088c0e4f670c3be0af725b7b710
jstahovic <jstahovicjr@gmail.com>
2019-01-13 23:34:56 -0500

    Tweaked RedModule's routing, explicitly adding the light-red route and having the RedModule's default route redirect to it.

--- src/app/red/red-routing.module.ts
+++ src/app/red/red-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { LightRedComponent } from './light-red/light-red.component'
 
-const routes: Routes = [{ path: '', component: LightRedComponent }]
+// const routes: Routes = [{ path: '', component: LightRedComponent }]
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'light-red' },
+  { path: 'light-red', component: LightRedComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
